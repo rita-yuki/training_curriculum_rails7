dce8fd9d994d6d16648d4a0152b25604da2948ae
rita-snow <yummy.standrews@gmail.com>
2024-10-07 17:47:40 +0900

    追加修正（空行の削除）

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -39,6 +39,5 @@ class CalendarsController < ApplicationController
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :wday => wdays[wday_num], :plans => today_plans}
       @week_days.push(days)
     end
-
   end
 end
