68259baf9784f778cd316c6f84f9363e88e86920
rita-snow <yummy.standrews@gmail.com>
2024-10-08 16:36:39 +0900

    Issue1,Issue3 の修正②

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -34,12 +34,11 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-
       wday_num = (@todays_date.wday + x) % 7
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :wday => wdays[wday_num], :plans => today_plans}
-
+      # add text
+      days = {month: (@todays_date + x).month, date: (@todays_date+x).day,wday: (wday_num), plans: today_plans}
       @week_days.push(days)
     end
+
   end
 end
